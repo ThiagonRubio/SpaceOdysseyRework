f8da48b052f0c20c15d95ee31cbd865806777828
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-10-30 16:34:08 -0300

    EnemySpawner se mueve

--- Assets/SpawnerController.cs
+++ Assets/SpawnerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpawnerController : MonoBehaviour
 {
+    private Vector3 _screenSpace;
+    
     [SerializeField] private EnemySpawner[] _spawners;
 
     [SerializeField] private float creationCooldown;
@@ -11,6 +13,7 @@ public class SpawnerController : MonoBehaviour
     
     void Start()
     {
+        _screenSpace = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
         _spawners = GetComponentsInChildren<EnemySpawner>();
         creationTime = creationCooldown;
     }
@@ -23,6 +26,13 @@ public class SpawnerController : MonoBehaviour
             int ran = Random.Range(0, _spawners.Length);
             _spawners[ran].Spawn();
             creationTime = creationCooldown;
+            ChangePosition();
         }
+    }
+
+    void ChangePosition()
+    {
+        var ran = Random.Range(-_screenSpace.y, _screenSpace.y);
+        transform.position = new Vector3(transform.position.x, ran, transform.position.z);
     }
 }
