7a377793766f7e311fe1b0649f8db7844ddb43b1
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-11-03 18:03:20 -0300

    Enemigo Amarillo (WIP)

--- Assets/Scripts/Entities/Enemies/YellowShip.cs
+++ Assets/Scripts/Entities/Enemies/YellowShip.cs
@@ -1,15 +1,14 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RedShip : Enemy, IMoveable, IAttacker
+public class YellowShip : Enemy, IMoveable, IAttacker
 {
     public float Speed => ActorStats.MovementSpeed;
-
+    
     public IWeapon[] Weapon => _weapons;
     public float AttackCooldownTimer => attackCooldownTimer;
-
+    
     public CommandEventQueue EntityCommandEventQueue => _entityCommandEventQueue;
     public CmdMove CmdMoveLeft => _cmdMoveLeft;
     public CmdMove CmdMoveRight => _cmdMoveRight;
@@ -23,31 +22,40 @@ public class RedShip : Enemy, IMoveable, IAttacker
     private CmdMove _cmdMoveUp;
     private CmdMove _cmdMoveDown;
     private CmdAttack _cmdAttack;
-
+    
     private IWeapon[] _weapons;
     private float attackCooldownTimer = 0;
+    
     [SerializeField] private float crashDamage;
+    
+    private Vector3 _screenSpace;
+    private SpriteRenderer _sprite;
+    private bool _isMovingUpwards;
     
     protected override void Start()
     {
         base.Start();
+        
+        //Los necesito para que hacer que no se salga de la pantalla
+        _screenSpace = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        _sprite = GetComponent<SpriteRenderer>();
+        
+        Debug.Log(_sprite.sprite.rect.height * 0.3 / 2);
+        
         SetWeaponToUse(GetComponentsInChildren<IWeapon>(true));
+        
         InitializeCommands();
     }
 
-    private void Update()
+    void Update()
     {
         Move();
-
+        ChangeDirection();
+        
         attackCooldownTimer += Time.deltaTime;
         if(attackCooldownTimer >= Weapon[0].FireRate) Attack();
     }
-
-    private void OnBecameInvisible()
-    {
-        OnPoolableObjectDisable();
-    }
-
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player") && other.gameObject.TryGetComponent<IDamageable>(out IDamageable damagedPlayer)) 
@@ -57,7 +65,7 @@ public class RedShip : Enemy, IMoveable, IAttacker
             Die();
         }
     }
-
+    
     public void InitializeCommands()
     {
         _entityCommandEventQueue = GetComponent<CommandEventQueue>();
@@ -73,6 +81,21 @@ public class RedShip : Enemy, IMoveable, IAttacker
     public void Move()
     {
         EntityCommandEventQueue.AddCommandToQueue(CmdMoveLeft, CommandEventQueue.UpdateFilter.Fixed);
+        
+        if(_isMovingUpwards) 
+            EntityCommandEventQueue.AddCommandToQueue(CmdMoveUp, CommandEventQueue.UpdateFilter.Fixed);
+        else 
+            EntityCommandEventQueue.AddCommandToQueue(CmdMoveDown, CommandEventQueue.UpdateFilter.Fixed);
+    }
+
+    private void ChangeDirection()
+    {
+        if (transform.position.y >= _screenSpace.y)
+            Debug.Log("Creo que pasó el borde de arriba");
+            _isMovingUpwards = true;
+        if (transform.position.y <= -_screenSpace.y)
+            Debug.Log("Creo que pasó el borde de abajo");
+            _isMovingUpwards = false;
     }
     
     public void SetWeaponToUse(IWeapon[] weaponsToUse)
