cb0b9f1b4ffd961d0f68f1d37e74b0a63344fdc1
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-11-04 18:04:21 -0300

    Cambios en el spawner

--- Assets/Scripts/Entities/Enemies/EnemySpawnerController.cs
+++ Assets/Scripts/Entities/Enemies/EnemySpawnerController.cs
@@ -1,35 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class EnemySpawnerController : MonoBehaviour, IListener
 {
     private Vector3 _screenSpace;
     
-    [SerializeField] private EnemySpawner[] _spawners;
+    [SerializeField] private EnemySpawner[] spawners;
+    [SerializeField] private EnemySpawner bossSpawner;
+    
+    [SerializeField] private float startingCreationCooldown;
+    [SerializeField] private float creationCooldownDecreasePerBoss;
+    private float _creationTime;
 
-    [SerializeField] private float creationCooldown;
-    private float creationTime;
+    [SerializeField] private int maxSpawnedAmount;
+    private int _spawnedAmount;
+
+    [SerializeField] private int difficulty;
+
+    [SerializeField] private int enemiesBetweenBosses;
+    private int _enemiesLeftTillBoss;
+    private bool _isBossSpawned;
     
     void Start()
     {
         _screenSpace = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
-        _spawners = GetComponentsInChildren<EnemySpawner>();
-        creationTime = creationCooldown;
+        spawners = GetComponentsInChildren<EnemySpawner>();
+        _creationTime = startingCreationCooldown;
+        _enemiesLeftTillBoss = enemiesBetweenBosses;
+        
+        EventManager.Instance.AddListener(EventConstants.EnemyDeath, this);
+        //EventManager.Instance.AddListener(EventConstants.BossDeath, this);
     }
 
     void Update()
     {
-        creationTime -= Time.deltaTime;
-        if (creationTime < 0)
+        _creationTime -= Time.deltaTime;
+    }
+
+    private void SpawnEnemy()
+    {
+        if (_creationTime < 0 && _spawnedAmount < maxSpawnedAmount && _enemiesLeftTillBoss != 0 && !_isBossSpawned)
         {
-            int ran = Random.Range(0, _spawners.Length);
-            _spawners[ran].Spawn();
-            creationTime = creationCooldown;
-            ChangePosition();
+            if (spawners.Length > difficulty)
+            {
+                int ran = Random.Range(0, difficulty);
+                spawners[ran].Spawn();
+                _spawnedAmount++;
+                _creationTime = startingCreationCooldown;
+                ChangePosition();
+            }
         }
     }
 
+    private void SpawnBoss()
+    {
+        if (_enemiesLeftTillBoss == 0 && !_isBossSpawned)
+        {
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            bossSpawner.Spawn();
+            startingCreationCooldown -= creationCooldownDecreasePerBoss;
+            _isBossSpawned = true;
+
+        }
+    }
+    
     void ChangePosition()
     {
         var ran = Random.Range(-_screenSpace.y, _screenSpace.y);
@@ -38,6 +74,10 @@ public class EnemySpawnerController : MonoBehaviour, IListener
 
     public void OnEventDispatch()
     {
-        //Disminuye la cantidad de enemigos spawneados, lo que permite spawnear más si llegó al límite
+        _spawnedAmount--;
+        _enemiesLeftTillBoss--;
+        
+        SpawnEnemy();
+        SpawnBoss();
     }
 }
---
+++ Assets/Scripts/Flyweight/EnemySpawnerStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySpawnerStats", menuName = "Stats/EnemySpawnerStats", order = 0)]
+
+public class EnemySpawnerStats
+{
+    
+}
