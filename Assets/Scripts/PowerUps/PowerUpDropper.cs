1c0a32420619c9c3b1c43a6d76da9f3df9669120
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-11-05 18:55:25 -0300

    PUDropper implementado (No funca)

--- Assets/Scripts/PowerUps/PowerUpDropper.cs
+++ Assets/Scripts/PowerUps/PowerUpDropper.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PowerUpDropper : MonoBehaviour, IListener //Lo deberían tener los enemy
 {
-    void Start()
-    {
-        EventManager.Instance.AddListener(EventConstants.EnemyDeath, this);
-    }
-   
+    [SerializeField] private GameObject[] powerUps;
+
+    [SerializeField] private int dropChancePercentage;
+    
     public void OnEventDispatch(string invokedEvent)
     {
-        //Random para saber si droppea
-        //Random para saber qué droppea
-        //Instanciar elemento a droppear
+        if (invokedEvent == EventConstants.EnemyDeath)
+        {
+            int ranA = Random.Range(0, 100);
+            if (ranA <= dropChancePercentage)
+            {
+                int ranB = Random.Range(0, powerUps.Length);
+                Instantiate(powerUps[ranB], transform.position, Quaternion.identity);
+            }
+        }
     }
 }
