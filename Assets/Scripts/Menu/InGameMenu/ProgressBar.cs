9705b6cd88cd49fa49aae2bc14d645fcf9b6b005
Facundo Cancela <facu.cancela.3@gmail.com>
2023-11-06 16:45:13 -0300

    ProgressBar

---
+++ Assets/Scripts/Menu/InGameMenu/ProgressBar.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour, IListener
+{
+    public Slider progressSlider;
+    public SpawnerControllerStats Stats => stats;
+    [SerializeField] private SpawnerControllerStats stats;
+
+    public float barPartition;
+    public float progress = 0;
+
+    public void Start()
+    {
+        EventManager.Instance.AddListener(EventConstants.EnemyDeath, this);
+        EventManager.Instance.AddListener(EventConstants.BossDeath, this);
+        barPartition = 1f / Stats.EnemiesBetweenBosses;
+
+        if (progressSlider != null)
+        {
+            progressSlider.value = progress;
+        }
+    }
+
+    public void OnEventDispatch(string invokedEvent)
+    {
+        if (invokedEvent == EventConstants.EnemyDeath)
+        {
+            progress += barPartition;
+            progress = Mathf.Clamp01(progress);
+
+            if (progressSlider != null)
+            {
+                progressSlider.value = progress;
+            }
+        }
+    }
+}
