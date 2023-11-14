7d715f8f4ab63c19a91866c2faefa38e49cf39e7
Facundo Cancela <facu.cancela.3@gmail.com>
2023-11-13 22:19:24 -0300

    Facades

--- Assets/Scripts/Menu/InGameMenu/HealthBar.cs
+++ Assets/Scripts/Menu/InGameMenu/HealthBar.cs
@@ -5,37 +5,16 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public GameObject healthBarPrefab;
-    public GridLayoutGroup healthBarParent;
-    public PlayerDamageableComponent PlayerDamageable;
-
-    private List<GameObject> healthBars = new List<GameObject>();
+    [SerializeField] private HealthBarFacade _healthBarFacade;
 
     private void Start()
     {
-        InitializeHealthBars();
+        _healthBarFacade.InitializeHealthBars();
     }
 
     private void Update()
     {
-        UpdateHealthBars();
-    }
-
-    void InitializeHealthBars()
-    {
-        for (int i = 0; i < PlayerDamageable.MaxHealth; i++)
-        {
-            GameObject healthBarInstance = Instantiate(healthBarPrefab, healthBarParent.transform);
-            healthBars.Add(healthBarInstance);
-        }
-    }
-
-    void UpdateHealthBars()
-    {
-        for (int i = 0;i < healthBars.Count ;i++)
-        {
-            healthBars[i].SetActive(i < PlayerDamageable.ActualHealth);
-        }
+        _healthBarFacade.UpdateHealthBars();
     }
 
 }
---
+++ Assets/Scripts/Menu/InGameMenu/ProgressBarFacade.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressBarFacade : MonoBehaviour, IListener
+{
+    public Slider progressSlider;
+    public SpawnerControllerStats Stats => stats;
+    [SerializeField] private SpawnerControllerStats stats;
+
+    public float barPartition;
+    public float progress = 0;
+
+    public void ProgressBarStart()
+    {
+        EventManager.Instance.AddListener(EventConstants.EnemyDeath, this);
+        EventManager.Instance.AddListener(EventConstants.BossDeath, this);
+        barPartition = 1f / Stats.EnemiesBetweenBosses;
+
+        if (progressSlider != null)
+        {
+            progressSlider.value = progress;
+        }
+    }
+
+    public void OnEventDispatch(string invokedEvent)
+    {
+        if (invokedEvent == EventConstants.EnemyDeath)
+        {
+            progress += barPartition;
+            progress = Mathf.Clamp01(progress);
+
+            if (progressSlider != null)
+            {
+                progressSlider.value = progress;
+            }
+        }
+
+        if (invokedEvent == EventConstants.BossDeath)
+        {
+            progress = 0;
+            progressSlider.value = progress;
+        }
+    }
+}
