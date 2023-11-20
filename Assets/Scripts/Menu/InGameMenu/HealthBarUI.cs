7b9c6cbbc639361298b7218c07305fe2ebb5a96f
ThiagonRubio <65839783+ThiagonRubio@users.noreply.github.com>
2023-11-20 01:15:30 -0300

    Dale Puto

--- Assets/Scripts/Menu/InGameMenu/HealthBarUI.cs
+++ Assets/Scripts/Menu/InGameMenu/HealthBarUI.cs
@@ -7,23 +7,18 @@ using UnityEngine.UI;
 public class HealthBarUI : MonoBehaviour
 {
     [SerializeField] private GameObject healthBarPrefab;
-    private GridLayoutGroup _healthBarParent;
-    private PlayerDamageableComponent _playerDamageable;
+    [SerializeField] private GridLayoutGroup _healthBarParent;
+    [SerializeField] private PlayerDamageableComponent _playerDamageable;
     private List<GameObject> _healthBars = new List<GameObject>();
-
-    private void Start()
-    {
-        _healthBarParent = GetComponent<GridLayoutGroup>();
-        _playerDamageable = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDamageableComponent>();
-    }
 
     public void InitializeHealthBars()
     {
-        for (int i = 0; i < _playerDamageable.MaxHealth; i++)
+        for (int i = 0; i < 10; i++)
         {
             GameObject healthBarInstance = Instantiate(healthBarPrefab, _healthBarParent.transform);
             _healthBars.Add(healthBarInstance);
         }
+        UpdateHealthBars();
     }
 
     public void UpdateHealthBars()
